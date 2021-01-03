39343a3e1d6b33c8a1c5d4da782daf3627d48ea7
pry0cc <pry0cc@0x00sec.org>
2021-01-03 02:21:36 +0000

    fixed styling

--- main.go
+++ main.go
@@ -1,54 +1,52 @@
 package main
 
 import (
-    "strings"
-    "os/exec"
-    "runtime"
-    "fmt"
-    "bufio"
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 	"sync"
-    "os"
 )
 
 var wg sync.WaitGroup
 
 func main() {
 	runtime.GOMAXPROCS(30)
-    os.Mkdir("output", 0755)
+	os.Mkdir("output", 0755)
 
-    sc := bufio.NewScanner(os.Stdin)
-    for sc.Scan() {
-        ipport := strings.ToLower(sc.Text())
-	    wg.Add(1)
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		ipport := strings.ToLower(sc.Text())
+		wg.Add(1)
 		go ScanLine(ipport, &wg)
-    }
+	}
 
-    wg.Wait()
+	wg.Wait()
 }
-
 
 // ScanLine takes input as ip:port and scans using nmap
 func ScanLine(ipport string, wg *sync.WaitGroup) (err error) {
 
-    defer wg.Done()
+	defer wg.Done()
 
-    s := strings.Split(ipport, ":")
+	s := strings.Split(ipport, ":")
 
-    ip := s[0]
-    port := s[1]
-    filename := fmt.Sprintf("output/%s_%s.xml", ip, port)
+	ip := s[0]
+	port := s[1]
+	filename := fmt.Sprintf("output/%s_%s.xml", ip, port)
 
-    fmt.Println("nmap -T4 -Pn -sV  -oX ", filename, " -p", port, " ", ip)
-    cmd := exec.Command("nmap", "-T4", "-sV",  "-Pn", "-oX",filename, "-p", port, ip)
-    
+	fmt.Println("nmap -T4 -Pn -sV  -oX ", filename, " -p", port, " ", ip)
+	cmd := exec.Command("nmap", "-T4", "-sV", "-Pn", "-oX", filename, "-p", port, ip)
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-    err = cmd.Run()
-    
-    if err != nil {
-        return
-    }
+	err = cmd.Run()
 
-    return nil
+	if err != nil {
+		return
+	}
+
+	return nil
 }
-
